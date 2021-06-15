c4aca65f1bc99ee42185cb77e34caf028cf7c030
DianaSanchez1103 <00188618@uca.edu.sv>
2021-06-15 15:58:59 -0600

    Labo 5 comentado y ordenado

--- Labo5/n.asm
+++ Labo5/n.asm
@@ -1,14 +1,9 @@
 org 100h
 
     section .text
-    xor AX, AX
-    xor SI, SI
-    xor BX, BX
-    XOR CX, CX
-    xor DX, DX
 
     MOV SI, 10h
-    MOV DI, 1d
+    MOV DI, 0d
     MOV CX, 4d
     
     call modotexto
@@ -25,72 +20,65 @@ org 100h
 
     movercursor:
         MOV AH, 02h ; posiciona el cursor en pantalla.
-        MOV DH, 8 ;fila en la que se mostrará el cursor
-        MOV DL, 20 ;columna en la que se mostrará el cursor
-        MOV BH, 0h ; número de página
-        INT 10h
-        RET
-
-    escribircaracter: ;utilizando interrupcion DOS
-        MOV AH, 09h
-        CMP DI, 1d
-        JE cade1
-        CMP DI, 2d
-        JE cade2
-        CMP DI, 3d
-        JE cade3
-        CMP DI, 4d
-        JE cade4
-        LOOP escribircaracter
-        RET
-
-     cade1:
-        ; escribe cadena en pantalla según posición del cursor
-        MOV DX, cadena1 ; cadena que vamos a imprimir, debe ir sin corchetes
-        INC DI
-        INT 21h
-        MOV AH, 02h ; posiciona el cursor en pantalla.
         MOV DH, 10 ;fila en la que se mostrará el cursor
         MOV DL, 20 ;columna en la que se mostrará el cursor
         MOV BH, 0h ; número de página
         INT 10h
-        JMP escribircaracter
+        RET
 
-     cade2:
-        ; escribe cadena en pantalla según posición del cursor
-        MOV DX, cadena2 ; cadena que vamos a imprimir, debe ir sin corchetes
-        INC DI
+    escribircaracter: 
+        MOV AH, 09h
+        INC DI ;incremento de DI
+        CMP DI, 1d ;Compara si DI(contador el ciclo el cual llega hasta 4(los 4 nombres)) es 1
+        JE cade1
+        CMP DI, 2d ;Compara si DI es 2
+        JE cade2
+        CMP DI, 3d ;Compara si DI es 3
+        JE cade3
+        CMP DI, 4d ;Compara si DI es 4
+        JE cade4
+        LOOP escribircaracter ;Salta a la funcion escribir caracter
+        RET
+
+    cade1:
+        MOV DX, cadena1 ; cadena que vamos a imprimir
         INT 21h
         MOV AH, 02h ; posiciona el cursor en pantalla.
         MOV DH, 12 ;fila en la que se mostrará el cursor
         MOV DL, 20 ;columna en la que se mostrará el cursor
         MOV BH, 0h ; número de página
         INT 10h
-        JMP escribircaracter
+        JMP escribircaracter ;Salta a la funcion escribir caracter
 
-    cade3:
-        ; escribe cadena en pantalla según posición del cursor
-        MOV DX, cadena3 ; cadena que vamos a imprimir, debe ir sin corchetes
-        INC DI
+    cade2:
+        MOV DX, cadena2 ; cadena que vamos a imprimir
         INT 21h
         MOV AH, 02h ; posiciona el cursor en pantalla.
         MOV DH, 14 ;fila en la que se mostrará el cursor
         MOV DL, 20 ;columna en la que se mostrará el cursor
         MOV BH, 0h ; número de página
         INT 10h
-        JMP escribircaracter
+        JMP escribircaracter ;Salta a la funcion escribir caracter
 
-    cade4:
-        ; escribe cadena en pantalla según posición del cursor
-        MOV DX, cadena4 ; cadena que vamos a imprimir, debe ir sin corchetes
-        INC DI
+    cade3:
+        MOV DX, cadena3 ; cadena que vamos a imprimir
         INT 21h
         MOV AH, 02h ; posiciona el cursor en pantalla.
         MOV DH, 16 ;fila en la que se mostrará el cursor
         MOV DL, 20 ;columna en la que se mostrará el cursor
         MOV BH, 0h ; número de página
         INT 10h
-        JMP escribircaracter
+        JMP escribircaracter ;Salta a la funcion escribir caracter
+
+    cade4:
+        MOV DX, cadena4 ; cadena que vamos a imprimir
+        INT 21h
+        MOV AH, 02h ; posiciona el cursor en pantalla.
+        MOV DH, 16 ;fila en la que se mostrará el cursor
+        MOV DL, 20 ;columna en la que se mostrará el cursor
+        MOV BH, 0h ; número de página
+        INT 10h
+        JMP escribircaracter ;Salta a la funcion escribir caracter
 
     esperartecla:
         MOV AH, 00h ; espera buffer del teclado para avanzar en la siguiente instrucción
@@ -102,7 +90,7 @@ org 100h
 
     section .data
 
-    cadena1 DB 'Diana$'
-    cadena2 DB 'Cristina$'
-    cadena3 DB 'Sanchez$'
-    cadena4 DB 'Munoz$'
+    cadena1 DB 'Diana$' ;Primer nombre
+    cadena2 DB 'Cristina$' ;Segundo nombre
+    cadena3 DB 'Sanchez$' ;Primero apellido
+    cadena4 DB 'Munoz$' ;Segundo apellido
